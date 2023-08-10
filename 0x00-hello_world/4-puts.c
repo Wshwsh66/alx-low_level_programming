4e5b07d9a95a3a11a343ea8a744517f9837f94a5
shimaa <wshwsh6@hotmail.com>
2023-08-10 08:07:13 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-/* main prints out the output */
+/**
+ *  main --prints out the output using puts
+ *
+ *  return always (0)
+ *
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
