90e1ab969329455e1d404a3de8d5705acd2b87c4
shimaa <wshwsh6@hotmail.com>
2023-08-10 08:13:50 -0700

    fix3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /**
  *  main --prints out the output using puts
  *
- *  return always (0)
+ *  Return always (0)
+ *
  */ 
-int main(void)
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
