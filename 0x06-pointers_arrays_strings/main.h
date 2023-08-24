453b9e2224c3ea73fc77f394080e00f698847008
shimaa <wshwsh6@hotmail.com>
2023-08-24 09:40:39 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,5 +2,13 @@
 #define _MAIN_H_
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+int _putchar(char c);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
 
 #endif
